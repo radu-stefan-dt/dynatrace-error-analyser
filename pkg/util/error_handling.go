63c61a4da4496fc0b0e0b82657bb182a5122d104
Radu <radu.stefan@dynatrace.com>
2021-04-16 09:10:26 +0100

    added checks for properties + error printing

--- pkg/util/error_handling.go
+++ pkg/util/error_handling.go
@@ -1,10 +1,36 @@
 package util
 
-import "os"
+import (
+	"errors"
+	"os"
+)
+
+func PrintErrors(errors []error) {
+	for _, err := range errors {
+		Log.Error("\t%s", err)
+	}
+}
 
 func FailOnError(err error, msg string) {
 	if err != nil {
 		Log.Fatal(msg + ": " + err.Error())
 		os.Exit(1)
 	}
+}
+
+func CheckProperty(properties map[string]string, property string) (string, error) {
+
+	prop, ok := properties[property]
+	if !ok {
+		return "", errors.New("Property " + property + " was not available")
+	}
+	return prop, nil
+}
+
+func CheckError(err error, msg string) bool {
+	if err != nil {
+		Log.Error(msg + ": " + err.Error())
+		return true
+	}
+	return false
 }
