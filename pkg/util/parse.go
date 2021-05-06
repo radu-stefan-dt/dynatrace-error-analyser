41f62ef47187c91c7a11b3ffa82da9e941df4ffa
Radu <radu.stefan@dynatrace.com>
2021-05-06 18:02:45 +0100

    added utility for sorting maps

--- pkg/util/parse.go
+++ pkg/util/parse.go
@@ -21,6 +21,7 @@ package util
 import (
 	"fmt"
 	"os"
+	"sort"
 	"strings"
 )
 
@@ -83,4 +84,28 @@ func UnpackSession(isLostBasket bool, session []interface{}) (sessionDetails map
 	sessionDetails["browserType"] = browserType
 
 	return sessionDetails
+}
+
+// Sort a map by its values
+type Pair struct {
+	Key   string
+	Value int
+}
+
+type PairList []Pair
+
+func (p PairList) Len() int           { return len(p) }
+func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p PairList) Less(i, j int) bool { return p[i].Value > p[j].Value }
+
+func SortMapDesc(m map[string]int) PairList {
+	p := make(PairList, len(m))
+	i := 0
+	for k, v := range m {
+		p[i] = Pair{k, v}
+		i++
+	}
+	sort.Sort(p)
+
+	return p
 }
